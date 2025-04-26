a6646d090d7a63cabd886d697f8939303aa9c59a
Blassel228 <hok.ykrop@gmail.com>
2025-04-26 21:56:39 +0300

    init

--- src/route/index.js
+++ src/route/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
 import Home from "@/pages/Home.vue";
 
 const routes = [{ path: "/", component: Home, name: "Home" }];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
